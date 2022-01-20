9812f3eee62f71caaa3325d696688af0904586a8
samsep101 <92789300+samsep101@users.noreply.github.com>
2022-01-20 15:35:04 +0500

    backend

--- backend/controllers/ApiController.php
+++ backend/controllers/ApiController.php
@@ -3,7 +3,10 @@
 namespace app\controllers;
 
 use Yii;
+use app\models\Users;
 use yii\rest\ActiveController;	
+use app\models\Login;
+
 
 class ApiController extends ActiveController
 {
@@ -19,7 +22,91 @@ class ApiController extends ActiveController
         // print_r($actions);
         return $actions;
     }
+	public function actionReg()
+	{
+		$post = Yii::$app->getRequest()->getBodyParams();
+        $model = new Users();
 
+        $model->login = $post['login']??'';
+        $model->password = $post['password']??'';
+        $model->rolekey = $post['rolekey']??'';
+        $model->description = $post['description']??'';
+        $model->username = $post['username']??'';
+
+        if ($model->validate()) {
+            $model->password = Yii::$app->getSecurity()->generatePasswordHash($model->password);
+
+            $model->api_key = Yii::$app->getRequest()->getCsrfToken();
+            $model->date = date('Y-m-d H:i:s');
+
+            $model->save();
+
+            return ['status'=>'204'];
+        } else {
+            $errors = [
+                'error'=>[
+                    'code'=>'422',
+                    'message'=> 'Validation error',
+                    'errors'=>[
+                        $model->errors
+                    ]
+                ]
+            ];
+            
+            return $errors;
+        }
+	}
+
+
+	public function actionLogin()
+    {   
+        $post = Yii::$app->getRequest()->getBodyParams();
+        $model = new Login();
+
+        $model->login = $post['login']??'';
+        $model->password = $post['password'] ?? '';
+        
+        
+        if ($model->validate()) {
+            if(Yii::$app->getSecurity()->validatePassword($post['password'], Users::findOne(['login'=>$post['login']])->password)){
+            
+
+                // $model->api_token = Yii::$app->getRequest()->getCsrfToken();
+
+				$user = Users::findOne(['login'=>$post['login']]);
+				$user->api_token =  Yii::$app->getRequest()->getCsrfToken();
+				$user->save();
+
+
+                return ['status'=>'204','token'=>$user->api_token];
+            }else{
+                $errors = [
+                    'error'=>[
+                        'code'=>'422',
+                        'message'=> 'Validation error',
+                        'errors'=>[
+                            $model->errors
+                        ]
+                    ]
+                ];
+                
+                return $errors;
+            }
+        } else {
+            $errors = [
+                'error'=>[
+                    'code'=>'422',
+                    'message'=> 'Validation error',
+                    'errors'=>[
+                        $model->errors
+                    ]
+                ]
+            ];
+            
+            return $errors;
+        }
+        
+    }
 	// public function beforeAction($action)
 	// {
 
