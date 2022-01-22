979dd8d6c4ba0c8d33cf3b05696273f638e731b0
samsep101 <92789300+samsep101@users.noreply.github.com>
2022-01-22 08:50:32 +0500

    js авторизация

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,33 +1,48 @@
 // (function(){
-    $('#signin').click(signIn);
+
+$(document).ready(function(){
+  $('#signin').click(signIn);
+});
 
 
 
-
-    function signIn(){
-        let login = $('#login').text()
-        let password = $('#password').text()
-
-        console.log(JSON.stringify(Fetch('http://192.168.102.115/api/login/', {'login':login,'password':password})));
-    }
-
-
-
-    async function Fetch(url = '', data = {}) {
-        // Default options are marked with *
-        const response = await fetch(url, {
-          method: 'POST',
-          mode: 'no-cors', // no-cors, *cors, same-origin
-        //   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        //   credentials: 'same-origin', // include, *same-origin, omit
-          headers: {
-            'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-          },
-        //   redirect: 'follow', // manual, *follow, error
-        //   referrerPolicy: 'no-referrer', // no-referrer, *client
-          body: JSON.stringify(data) // body data type must match "Content-Type" header
-        });
-        return await response.json(); // parses JSON response into native JavaScript objects
+function signIn(){
+    let login = $('#contact-1-name').val();
+    let password = $('#contact-1-password').val();
+    var returns;
+    let loginApi = Fetch('http://localhost:8080/api/login/', {'login':login,'password':password});
+    loginApi.then((data)=>{
+      if(data.status == '204'){
+        $('#menu').append('<li><a href="">Аккаунт</a></li>');
       }
+    });
+    
+    
+}
+
+
+
+async function Fetch(url = '', data = {}) {
+    // Default options are marked with *
+
+  
+  var formBody = [];
+  for (var property in data) {
+    var encodedKey = encodeURIComponent(property);
+    var encodedValue = encodeURIComponent(data[property]);
+    formBody.push(encodedKey + "=" + encodedValue);
+  }
+  formBody = formBody.join("&");
+    let response = await fetch(url, {
+      method: 'POST',
+      // mode:'no-cors',
+      headers: {
+        // 'Content-Type': 'application/json'
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+
+      body: formBody // body data type must match "Content-Type" header
+    });
+    return  await response.json();
+ }
       
